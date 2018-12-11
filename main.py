d3f7ef55cdf85076140d1e2aecdcda8ef0717e66
Juneld Nerosa <juneld.nerosa@detailonline.tech>
2018-12-11 17:59:51 +0800

    test 11

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 def hello_world():
   return 'Hey Its Python Flask application!!!'
 
-@app.route('/test')
-def hello_world2():
+@app.route('/awesome')
+def awesome():
   return 'Hey its Python Flask application Test!'
 
 if __name__ == '__main__':
