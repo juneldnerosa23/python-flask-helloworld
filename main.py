a2186eca0a2fcb0311474edb3a0c0c4044855c40
Juneld Nerosa <juneld.nerosa@detailonline.tech>
2018-12-11 16:27:41 +0800

    test9

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hey Its Python Flask application!'
+  return 'Hey Its Python Flask application!!!'
 
 @app.route('/test')
 def hello_world2():
